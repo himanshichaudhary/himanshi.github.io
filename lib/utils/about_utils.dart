c622ef3677f25b9bb44f881dd6e5bb6eb52f112f
developer <developer@sabbatech.com>
2025-09-26 17:08:05 +0530

    chore(pages): publish Flutter web build to root (user site)

--- lib/utils/about_utils.dart
+++
@@ -1,8 +0,0 @@
-
-class AboutUtils {
-  static const String aboutMeHeadline =
-      'I\'m Himanshi Chaudhary, a Flutter developer, Technical blog writer and UI designer.';
-
-  static const String aboutMeDetail =
-      'I\'m a Fresh Graduate with Computer Science from AKTU University, Lucknow. I have been developing mobile apps for over 5 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a former  Google Developer Student Clubs (DSC) lead and also CEO/Co-Founder Flutter Islamabad, Pakistan. Currently working as Associate Software Engineer at Sastaticket.pk';
-}
